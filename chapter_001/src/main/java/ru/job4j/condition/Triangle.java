b1b1ba3ac60621b00642d593679489ae71740ede
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-01-14 23:30:23 +0300

    Рефакторинг - Площадь треугольника.[#216188]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,35 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+
 public class Triangle {
-    public static boolean exist(double ab, double bc, double ca) {
-        return ab + bc > ca && bc + ca > ab && ca + ab > bc;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = second.distance(third);
+        double c = first.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
+    private boolean exist(double a, double c, double b) {
+        return a + b > c && b + c > a && c + a > b;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -7,13 +7,23 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 3.0, 3.0);
-        assertThat(result, is(true));
+        Point ap = new Point(0, 0);
+        Point bp = new Point(4, 0);
+        Point cp = new Point(0, 3);
+        Triangle abc = new Triangle(ap, bp, cp);
+        double expected = 6.0;
+        double out = abc.area();
+        assertThat(expected, is(out));
     }
 
     @Test
     public void whenDoesNotExist() {
-        boolean result = Triangle.exist(2.0, 3.0, 6.0);
-        assertThat(result, is(false));
+        Point ap = new Point(0, 0);
+        Point bp = new Point(4, 0);
+        Point cp = new Point(3, 0);
+        Triangle abc = new Triangle(ap, bp, cp);
+        double expected = -1;
+        double out = abc.area();
+        assertThat(expected, is(out));
     }
 }
