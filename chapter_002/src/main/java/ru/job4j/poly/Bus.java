fae7ace2932f502f67dd71656205b88426f0350d
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-01-27 22:00:31 +0300

    Интерфейс Input[#216168]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int quantity) {
+
+    }
+
+    @Override
+    public int fill(int fuel) {
+        return 0;
+    }
+}
