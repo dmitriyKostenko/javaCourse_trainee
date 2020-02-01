7a9ec5a62cf194300a5402003e6b380a17a85f71
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-02-01 23:46:05 +0300

    Написать тесты на StartUI.[#216178]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,6 +6,15 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
     @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        assertThat(action.isCall(), is(true));
+    }
+/*    @Test
     public void whenAddItem() {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
@@ -38,5 +47,5 @@ public class StartUITest {
         String[] answers = {item.getId()};
         StartUI.deleteItem(new StubInput(answers), tracker);
         assertNull(tracker.findById(item.getId()));
-    }
+    }*/
 }
