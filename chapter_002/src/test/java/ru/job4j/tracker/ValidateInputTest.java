6353ea3ea48d8971c87d864aebb9ddcfbae2e4e9
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-02-04 22:38:40 +0300

    Дополнение теста для исключения IllegalStateException

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -29,12 +29,12 @@ public class ValidateInputTest {
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
         ValidateInput input = new ValidateInput(
-                new StubInput(new String[] {"1", "2"})
+                new StubInput(new String[] {"7", "one", "5"})
         );
-        input.askInt("Enter", 1);
+        input.askInt("Enter", 6);
         assertThat(
                 mem.toString(),
-                is("Please select key from menu.\r\n")
+                is("Please select key from menu.\r\nPlease enter validate data again.\r\n")
         );
         System.setOut(out);
     }
