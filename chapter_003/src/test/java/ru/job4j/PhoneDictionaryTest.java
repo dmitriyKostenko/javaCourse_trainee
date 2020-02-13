6639b04caadb3f5bd80d812c7bb6344fd728ccbc
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-02-13 11:34:09 +0300

    Телефонный справочник на базе ArrayList[#216133]

---
+++ chapter_003/src/test/java/ru/job4j/PhoneDictionaryTest.java
@@ -0,0 +1,39 @@
+package ru.job4j;
+
+import org.junit.Test;
+import ru.job4j.search.Person;
+import ru.job4j.search.PhoneDictionary;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Pet");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByPartSurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("sent");
+        assertThat(persons.get(0).getName(), is("Petr"));
+    }
+
+    @Test
+    public void whenFindByPartPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("487");
+        assertThat(persons.get(0).getAddress(), is("Bryansk"));
+    }
+}
