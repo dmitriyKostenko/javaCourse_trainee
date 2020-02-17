fff33469e28d5506e0e871fb059cd3f2b26391a7
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-02-17 09:48:45 +0300

    Идентичные тексты.[#216143] // исправление метода isEquals и теста к нему

--- chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean result = false;
+        boolean result = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
@@ -12,8 +12,8 @@ public class UniqueText {
             check.add(word);
         }
         for (String word : text) {
-            if (check.contains(word)) {
-                result = true;
+            if (!check.contains(word)) {
+                result = false;
                 break;
             }
         }
