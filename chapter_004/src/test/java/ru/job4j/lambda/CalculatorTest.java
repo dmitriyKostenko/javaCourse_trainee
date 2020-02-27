b35c07890581e337728203d427ced601928e2e39
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-02-27 14:56:39 +0300

    Ссылки на методы.[#216004]

--- chapter_004/src/test/java/ru/job4j/lambda/CalculatorTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
         List<Double> buffer = new ArrayList<>();
         calc.multiple(0, 3, 1,
                 (value, index) -> (double) value + index,
-                result -> buffer.add(result)
+                buffer::add
         );
         assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
     }
@@ -25,7 +25,7 @@ public class CalculatorTest {
         List<Double> buffer = new ArrayList<>();
         calc.multiple(0, 3, 1,
                 (value, index) -> (double) value * index,
-                result -> buffer.add(result)
+                buffer::add
         );
         assertThat(buffer, is(Arrays.asList(0D, 1D, 2D)));
     }
