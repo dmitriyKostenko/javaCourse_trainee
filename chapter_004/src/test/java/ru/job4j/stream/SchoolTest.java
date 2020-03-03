72166b80ca8b68e5f062e0b7e95b1edf36820ff8
Dmitrii Kostenko <kdm.tag@gmail.com>
2020-03-03 15:52:44 +0300

    Фильтрация учеников.[#216010]

---
+++ chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class SchoolTest {
+    List<Student> students = Arrays.asList(
+            new Student("Antonov", 20),
+            new Student("Borisov", 35),
+            new Student("Vasilev", 45),
+            new Student("Konstantinov", 55),
+            new Student("Nikolaev", 65),
+            new Student("Yuriev", 75),
+            new Student("Yakovlev", 90)
+    );
+
+    @Test
+    public void whenSortedToClass10A() {
+        School school = new School();
+        List<Student> result = school.collect(students,
+                student -> student.getScore() >= 70 && student.getScore() <= 100);
+        List<Student> expect = Arrays.asList(
+                new Student("Yuriev", 75),
+                new Student("Yakovlev", 90)
+        );
+        assertThat(result.toString(), is(expect.toString()));
+    }
+
+    @Test
+    public void whenSortedToClass10B() {
+        School school = new School();
+        List<Student> result = school.collect(students,
+                student -> student.getScore() >= 50 && student.getScore() < 70);
+        List<Student> expect = Arrays.asList(
+                new Student("Konstantinov", 55),
+                new Student("Nikolaev", 65)
+        );
+        assertThat(result.toString(), is(expect.toString()));
+    }
+
+    @Test
+    public void whenSortedToClass10V() {
+        School school = new School();
+        List<Student> result = school.collect(students,
+                student -> student.getScore() >= 0 && student.getScore() < 50);
+        List<Student> expect = Arrays.asList(
+                new Student("Antonov", 20),
+                new Student("Borisov", 35),
+                new Student("Vasilev", 45)
+        );
+        assertThat(result.toString(), is(expect.toString()));
+    }
+}
